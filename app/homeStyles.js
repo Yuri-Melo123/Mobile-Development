a1bb8a85c353732d4bfc2926f7c7c0ed6b019b5c
Yuri-Melo123 <yuri.melo93503@a.fecaf.com.br>
2026-03-11 19:31:56 -0300

    Update homeStyles.js

--- app/homeStyles.js
+++ app/homeStyles.js
@@ -7,13 +7,11 @@ export const styles = StyleSheet.create({
     padding: 20,
     marginTop: 40
   },
-
   nav: {
     flexDirection: "row", 
     justifyContent: "space-between", 
     marginBottom: 10 
   },
-
   navButton: {
     backgroundColor: "#2f95dc",
     color: "#fff",
@@ -24,50 +22,42 @@ export const styles = StyleSheet.create({
     marginBottom: 20,
     width: "100%"
   },
-
   label: {
     fontSize: 18,
     marginBottom: 10
   },
-
   input: {
     borderWidth: 1,
     padding: 10,
     borderRadius: 8,
     marginBottom: 10
   },
-
   button: {
     backgroundColor: "#2f95dc",
     padding: 12,
     borderRadius: 8,
     marginBottom: 20
   },
-
   buttonText: {
     color: "#fff",
     textAlign: "center",
     fontWeight: "bold"
   },
-
   card: {
     flex: 1,
     margin: 5,
     alignItems: "center"
   },
-
   image: {
     width: 150,
     height: 150,
     borderRadius: 10
   },
-
   title: {
     fontSize: 14,
     marginTop: 5,
     textAlign: "center"
   },
-
   price: {
     fontWeight: "bold"
   },
